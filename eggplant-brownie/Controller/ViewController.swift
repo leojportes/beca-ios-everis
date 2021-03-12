ab8aef0890af3c4a2b9e593a5c002e86345a7067
leojportes <leojportes@gmail.com>
2021-03-12 01:42:03 -0300

    :sparkles: Declarei variáveis aos Inputs

--- eggplant-brownie/Controller/ViewController.swift
+++ eggplant-brownie/Controller/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var NomeTextField: UITextField!
+    @IBOutlet weak var FelicidadeTextField: UITextField!
+    
+    @IBAction func adicionar(_ sender: Any) {
+        var Nome = NomeTextField.text
+        
+        var Felicidade = FelicidadeTextField.text
+        
+        print("Comi \(Nome) e fiquei com felicidade: \(Felicidade) ")
     }
-
-
+    
 }
-
