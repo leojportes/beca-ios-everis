669f1c5b51196e8b7e185dbd55919e4904f52a8b
leojportes <leojportes@gmail.com>
2021-03-17 10:22:06 -0300

    :sparkless: Gesture, longPress e casting

--- eggplant-brownie/Controllers/RefeicoesTableViewController.swift
+++ eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -21,13 +21,29 @@ class RefeicoesTableViewController: UITableViewController, AdicionarRefeicaoDele
             let refeicao = refeicoes[indexPath.row]
             let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
             celula.textLabel?.text = refeicao.nome
-        
+            
+            let longPress = UILongPressGestureRecognizer(target: self, action: #selector(mostrarDetalhes(_:)))
+            
+            celula.addGestureRecognizer(longPress)
+            
             return celula
         }
     
         func add(_ refeicao: Refeicao) {
             refeicoes.append(refeicao)
             tableView.reloadData()
+        }
+    
+    @objc func mostrarDetalhes(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            let celula = gesture.view as! UITableViewCell
+            
+            guard let indexPath = tableView.indexPath(for: celula) else { return }
+            
+            let refeicao = refeicoes[indexPath.row]
+            
+            print("refeicao: \(refeicao.nome)")
+            }
         }
 
         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
